71089bba298adcdf325acaa8b4154fd5f59e356a
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-14 13:05:38 +0000

    resturant migration

--- backend/core/resturant/models.py
+++ backend/core/resturant/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-        description = models.TextField(blank=True)
-            price = models.DecimalField(max_digits=6, decimal_places=2)
-                available = models.BooleanField(default=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)    
+    available = models.BooleanField(default=True)
 
-                    def __str__(self):
-                            return self.name
+    def __str__(self):
+       return self.name
 
 # Create your models here.
