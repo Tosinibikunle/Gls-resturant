34c17b206a103f5e8a80896eaf7735e0d3b85a3b
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-24 19:21:19 +0000

    view corrections

--- backend/core/accounts/views.py
+++ backend/core/accounts/views.py
@@ -14,17 +14,18 @@ class RegisterView(APIView):
           return Response({'message': 'User created'}, status=status.HTTP_201_CREATED)
        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-                                                    class LoginView(APIView):
-                                                        def post(self, request):
-                                                                username = request.data.get('username')
-                                                                        password = request.data.get('password')
-                                                                                user = authenticate(request, username=username, password=password)
-                                                                                        if user:
-                                                                                                    login(request, user)
-                                                                                                                return Response({'message': 'Logged in'}, status=status.HTTP_200_OK)
-                                                                                                                        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
-                                                                                                                        class LogoutView(APIView):
+class LoginView(APIView):
+    def post(self, request):
+       username = request.data.get('username')
+       password = request.data.get('password')
+       user = authenticate(request, username=username, password=password)
+       if user:
+         login(request, user)
+         return Response({'message': 'Logged in'}, status=status.HTTP_200_OK)
+       return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
                                                                                                                             permission_classes = [IsAuthenticated]
 
                                                                                                                                 def post(self, request):
