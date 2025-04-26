88660ed517ce51f2720fbc946714ea4a22601d2d
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-26 10:28:28 +0000

    serial correct

--- backend/core/accounts/serializers.py
+++ backend/core/accounts/serializers.py
@@ -4,16 +4,16 @@ from django.contrib.auth.password_validation import validate_password
 
 class SignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-        password2 = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
 
-            class Meta:
-                    model = User
-                            fields = ('username', 'email', 'password', 'password2')
+  class Meta:
+       model = User
+       fields = ('username', 'email', 'password', 'password2')
 
-                                def validate(self, attrs):
-                                        if attrs['password'] != attrs['password2']:
-                                                    raise serializers.ValidationError({"password": "Passwords do not match."})
-                                                            return attrs
+       def validate(self, attrs):
+          if attrs['password'] != attrs['password2']:
+              raise serializers.ValidationError({"password": "Passwords do not match."})
+          return attrs
 
                                                                 def create(self, validated_data):
                                                                         validated_data.pop('password2')
