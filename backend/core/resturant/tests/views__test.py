4097563a3876cbf680f6847d13c93110223250d8
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-03 16:18:18 +0000

    resturant views test

---
+++ backend/core/resturant/tests/views__test.py
@@ -0,0 +1,17 @@
+from rest_framework.test import APIClient
+from django.test import TestCase
+from restaurant.models import MenuItem
+
+class MenuItemViewTests(TestCase):
+    def setUp(self):
+            self.client = APIClient()
+
+                def test_list_menu_items(self):
+                        MenuItem.objects.create(name='Burger', price=1200, available=True)
+                                response = self.client.get('/api/restaurant/menu/')
+                                        self.assertEqual(response.status_code, 200)
+
+                                            def test_create_menu_item(self):
+                                                    data = {'name': 'Pizza', 'price': 2000, 'available': True}
+                                                            response = self.client.post('/api/restaurant/menu/', data)
+                                                                    self.assertEqual(response.status_code, 201)
