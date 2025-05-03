1e9c6ee46144370a96f7f85ba135f773a2617ed9
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-03 16:26:01 +0000

    resturant test mod

--- backend/core/resturant/tests/views__test.py
+++ backend/core/resturant/tests/views__test.py
@@ -4,14 +4,14 @@ from restaurant.models import MenuItem
 
 class MenuItemViewTests(TestCase):
     def setUp(self):
-            self.client = APIClient()
+       self.client = APIClient()
 
-                def test_list_menu_items(self):
-                        MenuItem.objects.create(name='Burger', price=1200, available=True)
-                                response = self.client.get('/api/restaurant/menu/')
-                                        self.assertEqual(response.status_code, 200)
+    def test_list_menu_items(self):
+        MenuItem.objects.create(name='Burger', price=1200, available=True)
+        response = self.client.get('/api/restaurant/menu/')
+        self.assertEqual(response.status_code, 200)
 
-                                            def test_create_menu_item(self):
-                                                    data = {'name': 'Pizza', 'price': 2000, 'available': True}
-                                                            response = self.client.post('/api/restaurant/menu/', data)
-                                                                    self.assertEqual(response.status_code, 201)
+    def test_create_menu_item(self):
+        data = {'name': 'Pizza', 'price': 2000, 'available': True}
+        response = self.client.post('/api/restaurant/menu/', data)
+        self.assertEqual(response.status_code, 201)
